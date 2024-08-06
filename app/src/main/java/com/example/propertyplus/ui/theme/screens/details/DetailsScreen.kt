a786efe76fbcecaac344ba65c321eb9d593f1f19
Njirainianthony <njirainianthony@gmail.com>
2024-08-06 10:16:44 +0300

    commit

--- app/src/main/java/com/example/propertyplus/ui/theme/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/propertyplus/ui/theme/screens/details/DetailsScreen.kt
@@ -4,21 +4,34 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,6 +48,8 @@ fun DetailsScreen(navController: NavController){
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
+
+
 
         TopAppBar(
 
@@ -73,9 +88,62 @@ fun DetailsScreen(navController: NavController){
                 modifier = Modifier.fillMaxSize()
             )
 
-            Text(text = "Wagwan")
+
+
+
+
+
+
+            Spacer(modifier = Modifier.height(20.dp))
 
         }
+
+        //SEARCHBAR
+        var search by remember { mutableStateOf("")}
+
+        OutlinedTextField(
+            value = search,
+            onValueChange ={search= it},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "search")},
+            placeholder = { Text(text = "Choose your location")}
+        )
+
+        //END OF SEARCHBAR
+
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+        Row {
+            //CARD 1
+
+            Card(
+                modifier = Modifier.height(180.dp).width(200.dp)
+            ) {
+
+                Box (modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+
+                    Image(
+                        painter = painterResource(id = R.drawable.homeimage),
+                        contentDescription ="home",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
+
+            }
+
+
+        }
+
+
+
+
 
     }
 
